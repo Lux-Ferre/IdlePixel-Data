ad46a22d0891d0bb6d3b3ea8e1844de7e29a2937
Lux-Ferre <Lux-Ferre@users.noreply.github.com>
2024-03-18 20:14:28 +0000

    models for repo and admin route
    NewUser model for getting data for adding a new user.
    
    TCGTableRow for parsing response for game_tcg table requests.

--- models.py
+++ models.py
@@ -1,7 +1,24 @@
 from pydantic import BaseModel
+from datetime import datetime
+
+
+# Admin
+class NewUser(BaseModel):
+    username: str
+    permissions: list[str | None]
+    allow_admin: bool = False
+
+
+# Repo
+class TCGTableRow(BaseModel):
+    datetime: datetime
+    holo: bool
+    id: int
+    name: str
+    player_id: int
 
 
 # TCG
 class CardName(BaseModel):
-    id: str
+    id: int
     name: str
