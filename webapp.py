c6d3d79a4d0635647d38e23b258ccf2b18ea13ff
Lux-Ferre <Lux-Ferre@users.noreply.github.com>
2024-03-20 21:57:04 +0000

    basic CORS support

--- webapp.py
+++ webapp.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from routers import tcg, admin
 
@@ -9,6 +10,21 @@ app = FastAPI(
         404: {"description": "Not found"},
     },
     root_path="/api"
+)
+
+origins = [
+    "http://localhost",
+    "http://localhost:8042",
+    "https://data.idle-pixel.com/",
+    "https://idle-pixel.com"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(tcg.router)
